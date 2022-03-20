03bc94a7b49e2132e5f100a653f150843429e156
faizanMayo786 <faizangame998@gmail.com>
2022-03-20 23:49:23 +0500

    Project start

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'PAL/Screens/LoginScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: LoginScreen(key: key,)
+    );
+  }
+}
