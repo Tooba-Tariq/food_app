28f1b7481a87560fe88112a6a746f44d67d72384
faizanMayo786 <faizangame998@gmail.com>
2022-05-26 22:04:57 +0500

    Code Refactoring

--- lib/src/screens/startscreen/start_screen.dart
+++ lib/src/screens/startscreen/start_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:food_app/core/util/logo_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../core/util/gredient_circle.dart';
+import '../../widget/startscreenwidget/overlay_start_screen.dart';
+
 class StartScreen extends StatefulWidget {
   const StartScreen({Key? key}) : super(key: key);
 
@@ -14,73 +17,17 @@ class _StartScreenState extends State<StartScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      bottom: 50,
-                      top: 75,
-                    ),
-                    child: Logo(
-                      height: 100,
-                      width: 100,
-                    ),
-                  ),
-                  Text(
-                    "Welcome!",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 27.0,
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    "\nEat Food App allow you to find best \n foods and restaurant around you!",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 13.0,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: -100,
+              left: -100,
+              child: GradientCircle(
+                radius: 150,
               ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 25,
-                  right: 25,
-                  bottom: 50,
-                ),
-                child: Container(
-                  height: 50,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Don't Wait, Get Started!",
-                          style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 17.0,
-                            color: const Color(0XFFF2A902),
-                          ),
-                        ),
-                        const Icon(
-                          Icons.arrow_forward,
-                          color: Color(0XFFF2A902),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            OverlayWidget(),
+          ],
         ),
       ),
     );
---
+++ lib/src/widget/startscreenwidget/overlay_start_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../core/util/logo_widget.dart';
+
+class OverlayWidget extends StatelessWidget {
+  const OverlayWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  bottom: 50,
+                  top: 75,
+                ),
+                child: Logo(
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Text(
+                "Welcome!",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 28.0,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                "\nEat Food App allow you to find best \n foods and restaurant around you!",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 25,
+              right: 25,
+              bottom: 50,
+            ),
+            child: Container(
+              height: 50,
+              child: TextButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "Don't Wait, Get Started!",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18.0,
+                            color: const Color(0XFFF2A902),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.arrow_forward,
+                      color: Color(0XFFF2A902),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
